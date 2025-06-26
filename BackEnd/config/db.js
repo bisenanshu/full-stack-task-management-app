995b8d7530c5da4cbf3f1263176b9063fcf69759
Anshu Bisen <bisenanshu48@gmail.com>
2025-06-26 23:28:03 +0530

    data

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -3,6 +3,9 @@
 import mongoose  from "mongoose";
 
 export const connectDB = async ()=> {
-   await mongoose.connect('mongodb+srv://bisenanshu48:anshu123@cluster0.buphz.mongodb.net/cuisine-courier').then(()=> console.log("DB Connected"));
+   // await mongoose.connect('mongodb+srv://bisenanshu48:anshu123@cluster0.buphz.mongodb.net/cuisine-courier').then(()=> console.log("DB Connected"));
+   await mongoose.connect('mongodb+srv://ambarishdandekar:8830699474@cluster0.uiuipzs.mongodb.net/cuisine-courier').then(()=> console.log("DB Connected"));
 
 }  
+
+
